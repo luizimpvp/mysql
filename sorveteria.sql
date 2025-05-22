1e12c95210929f9a488dcff1f717a59c3f855ac4
luizimpvp <luizimreidelas157w@gmail.com>
2025-05-22 16:33:20 -0300

    Add files via upload

---
+++ sorveteria.sql
@@ -0,0 +1,28 @@
+create table sorvete(
+   id int primary key auto_increment,
+   nome varchar(10) not null,
+   fabricante varchar(10) not null
+   );
+show databases;
+insert into sorvete valuies ( null, "chocolate", "caseiro");
+insert into sorvete valuies ( null, "flocos", "yopa");
+insert into sorvete valuies( null, "morango", "yopa");
+insert into sorvete valuies( null, "chocolate", "kibon");
+insert into sorvete valuies( null, "chocolate", "yopa");
+ select * from
+select * from sorvete wehere nome = "chocolate"
+criar uma tabela de cliente para cadrastrar quem compra na sorveteria  
+ create table cliente(
+	id cliente int primary key auto_insert
+	nome varchar (10) not null
+	sexo enum ('f', 'm') not null
+	idade int
+);
+select * from cliente;
+insert into cliente values (null, 'thiago', 'm', '16');
+insert into cliente values (null, 'simone', 'f', '16');
+insert into cliente values (null, 'aurea', 'f', '16');
+insert into cliente values (null, 'angelo', 'm', '16');
+insert into cliente values (null, 'luiz', 'm', '16');
+desc cliente;
+select * fromcliente
